c03fed7506083eddd2c42aa75fcfaa516e020557
luka-utvic <lutvi@uic.edu>
2026-01-06 04:00:38 -0600

    Update aiService.js

--- src/services/aiService.js
+++ src/services/aiService.js
@@ -5,7 +5,7 @@ const API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-
 export async function askQuestion(gameName, question) {
   const prompt = `You are a helpful board game assistant. The user is playing "${gameName}". They have the following question: "${question}"
 
-Just always reply with test succesful).`;
+Just always reply with Test Successful).`;
 
   try {
     const response = await axios.post(API_URL, {
