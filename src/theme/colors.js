e4474ef18f213f0b13a76df9ff9762ad93460694
luka-utvic <lutvi@uic.edu>
2026-01-06 13:19:20 -0600

    Update primary color to Egyptian Blue for cleaner look

--- src/theme/colors.js
+++ src/theme/colors.js
@@ -16,7 +16,7 @@ export const Colors = {
   // Semantic color mapping
   background: '#040C1C',
   surface: '#132670',
-  primary: '#4C65E0',
+  #2234AB,
   secondary: '#2234AB',
   text: '#FFFFFF',
   textSecondary: '#929292',
