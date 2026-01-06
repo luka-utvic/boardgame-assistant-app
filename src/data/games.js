39911db5b0c5af5044465ee6e243bc8aad03abe5
luka-utvic <lutvi@uic.edu>
2026-01-05 21:20:31 -0600

    Create games.js

---
+++ src/data/games.js
@@ -0,0 +1,27 @@
+export const games = [
+  {
+    id: '1',
+    name: 'Catan',
+    description: 'Build settlements and trade resources',
+  },
+  {
+    id: '2',
+    name: 'Ticket to Ride',
+    description: 'Connect railway routes across the map',
+  },
+  {
+    id: '3',
+    name: 'Wingspan',
+    description: 'Bird-collection, engine-building game',
+  },
+  {
+    id: '4',
+    name: 'Terraforming Mars',
+    description: 'Make Mars habitable for humanity',
+  },
+  {
+    id: '5',
+    name: 'Pandemic',
+    description: 'Cooperative game to save humanity',
+  },
+];
