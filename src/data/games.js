25571c83202f633f250f9db37f7b931666fcdb40
luka-utvic <lutvi@uic.edu>
2026-01-06 22:09:48 -0600

    Update games.js

--- src/data/games.js
+++ src/data/games.js
@@ -24,4 +24,39 @@ export const games = [
     name: 'Pandemic',
     description: 'Cooperative game to save humanity',
   },
+   {
+    id: '6',
+    name: 'Catan',
+    description: 'Build settlements and trade resources',
+  },
+  {
+  id: '6',
+  name: 'Carcassonne',
+  description: 'Place tiles to build medieval landscapes',
+},
+{
+  id: '7',
+  name: 'Azul',
+  description: 'Draft tiles to decorate a royal palace',
+},
+{
+  id: '8',
+  name: 'Scythe',
+  description: 'Alternate-history strategy with mechs and resources',
+},
+{
+  id: '9',
+  name: '7 Wonders',
+  description: 'Develop a civilization through card drafting',
+},
+{
+  id: '10',
+  name: 'The Crew',
+  description: 'Cooperative trick-taking space mission game',
+},
+{
+  id: '11',
+  name: 'Root',
+  description: 'Asymmetric woodland warfare and strategy',
+}
 ];
